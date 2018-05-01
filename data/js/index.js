77e89dbd52ab1378f36a77a6cb31d31e61d48e91
maximilián <maliarmaximilian@gmail.com>
2018-05-01 13:36:37 +0200

    started

---
+++ data/js/index.js
@@ -0,0 +1,160 @@
+function add(pocet, id, toNull) {
+    toNull.value= '';
+    xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/addToCart');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+        loadKosik();
+    };
+    xhr.send(encodeURI('id=' + id + "&pocet=" + pocet)); 
+
+    var anim = toNull.parentElement.parentElement.children[0];
+    var clone = anim.cloneNode(true);
+    var rect = anim.getBoundingClientRect();
+    clone.style.position = "fixed";
+    clone.style.top = rect.top+'px';
+    clone.style.left = rect.left+'px';
+    clone.className += " animate";
+    var res = anim.parentElement;
+    res.appendChild(clone);
+    setTimeout(()=>{
+        res.removeChild(res.lastChild); 
+    }, 300);
+}
+
+function rem(pocet, id) {
+    xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/remFromCart');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status !== 200) {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+        
+        loadKosik();
+    };
+    xhr.send(encodeURI('id=' + id + "&pocet=" + pocet)); 
+}
+
+function loadKosik(){
+    updatePocet();
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               document.getElementById("vkosiku").innerHTML = xmlhttp.responseText;
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "/kosikUp", true);
+    xmlhttp.send();
+}
+
+function updatePocet() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               if(xmlhttp.responseText > 0) document.getElementById("kosikIMG").innerHTML = '<div id="kosikPocet">' + xmlhttp.responseText + '</div>';
+               else document.getElementById("kosikIMG").innerHTML ="";
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "/kosikPocet", true);
+    xmlhttp.send();
+}
+
+ var utcSeconds = 1417903843000,
+     d = new Date();
+ 
+$( function() {
+    $( "#date" ).datepicker();
+    $( "#date" ).datepicker("option", "minDate", tomorrow());
+    $( "#date" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+    $( "#date" ).datepicker( "option", "dayNames", ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok", "Sobota" ] );
+    $( "#date" ).datepicker( "option", "dayNamesMin", [ "Ne", "Po", "Ut", "St", "Št", "Pi", "So" ] );
+    $( "#date" ).datepicker( "option", "firstDay", 1 );
+});
+
+function tomorrow() {
+    var p2 = new Date().getTime() + (18 * 3600 * 1000 );
+    var today = new Date(p2);
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    return new Date(yyyy + "-" + mm + "-" + dd);   
+}
+
+
+
+
+var kosikON = false
+window.onclick = function(event) { 
+    var isKosik = containClass(event.path, "kosik");
+    if(isKosik){
+        if(!kosikON){
+            kosikON = true;
+            updateKosik(kosikON);
+        }
+        kosikON = true;
+        updateKosik(kosikON);
+    }
+    else{
+        if(kosikON){
+            kosikON = false;
+            updateKosik(kosikON);
+        }
+    }
+}
+
+function containClass(path, find){
+    var found = false;
+    path.forEach((element)=>{
+        if(typeof element.className != "undefined" && element.className == find) {
+            found = true;
+        }
+    });
+    return found;
+}
+
+function updateKosik(display){
+    var vkosiku = document.getElementById("vkosiku");
+    if(display)vkosiku.style.display = "block";
+    else vkosiku.style.display = "none";
+}
+
+function objednaj(){
+    var current = window.location.href;
+    if(current[current.length-1] != "/") window.location.href = current + "/objednavka";
+    else window.location.href = current + "objednavka";
+}
---
+++ main.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const fs = require("fs");
+var helmet = require('helmet');
+var session = require('express-session');
+//let sseExpress = require('sse-express');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(helmet());
+app.use(session({
+    secret: 'afasfsafasfazvxcbgnvhmbjlyhfvhndfgvxc',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(bodyParser.json()); //Parses the text as JSON and exposes the resulting object on req.body.
+app.use('/data/', express.static('data'));
+app.listen(80, ()=>{console.log("server started!!")});
+/*
+Schema :  Meno, obrazok, popis, input:{pocet}, cena, datum donesenia {HTML CALENDAR}
+*/
+app.get('/nakup', (req, res)=>{
+    if(typeof req.session.kosik == "undefined") req.session.kosik = [];
+    var kolace = JSON.parse(fs.readFileSync('./vsetky.json'));
+    res.render("nakup", {"kosik": req.session.kosik, "kolace": kolace});
+});
+app.get('/', (req, res)=>{
+    res.render("index", {});
+});
+
+app.get('/kosikUp/', (req, res)=> {
+    var kolace = JSON.parse(fs.readFileSync('./vsetky.json'));
+    res.render("parts/kosik", {"kosik": req.session.kosik, "kolace": kolace});
+});
+
+app.post('/addToCart/', (req,res)=>{
+    if(typeof req.body.id != "undefined" && typeof req.body.pocet != "undefined" && Math.floor(Number(req.body.pocet)) > 0) {
+        if( typeof req.session.kosik != "undefined"){
+            var existuje = "nie";
+            req.session.kosik.forEach((item, index) => {
+                if(item.id == req.body.id){
+                    existuje = index;
+                }
+            });
+            if(existuje !== "nie"){
+                var cislo = Number(req.session.kosik[existuje].pocet);
+                req.session.kosik[existuje].pocet = Math.floor(Number(req.body.pocet)) + cislo;
+            }
+            else{
+                req.session.kosik.push({"id": req.body.id, "pocet": Math.floor(Number(req.body.pocet)) });
+            }
+            
+        }
+        else{
+            req.session.kosik = [{"id": req.body.id, "pocet": Math.floor(Number(req.body.pocet))}];
+        }
+    }
+    res.end();
+});
+
+app.post('/remFromCart/', (req,res)=>{
+    if(typeof req.body.id != "undefined" && typeof req.body.pocet != "undefined" && Math.floor(Number(req.body.pocet).toFixed(0)) > 0) {
+        if( typeof req.session.kosik != "undefined"){
+            var existuje = "nie";
+            req.session.kosik.forEach((item, index) => {
+                if(item.id == req.body.id){
+                    existuje = index;
+                }
+            });
+            if(existuje !== "nie"){
+                var cislo = Number(req.session.kosik[existuje].pocet);
+                req.session.kosik[existuje].pocet = cislo - Math.floor(Number(req.body.pocet));
+                if(req.session.kosik[existuje].pocet < 1){
+                    req.session.kosik.splice(existuje, 1);
+                }
+            }       
+        }
+    }
+    res.end();
+});
+
+app.get('/kosikPocet/', (req, res)=>{
+
+    res.end(String(req.session.kosik.length));
+});
+
+app.get('/nakup/objednavka', (req, res)=>{
+    if(typeof req.session.kosik == "undefined") req.session.kosik = [];
+    var kolace = JSON.parse(fs.readFileSync('./vsetky.json'));
+    res.render("objednaj", {"kosik": req.session.kosik, "kolace": kolace, "NOkosik": true});
+});
