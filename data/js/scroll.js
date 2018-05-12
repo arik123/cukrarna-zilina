35b81d90c4f8dd301838a6ff1f58b3a69e722b9c
maximilián <maliarmaximilian@gmail.com>
2018-05-12 13:13:33 +0200

    fixed navbar

--- data/js/scroll.js
+++ data/js/scroll.js
@@ -24,15 +24,15 @@ window.addEventListener('scroll', ()=>{
     scroll = window.scrollY;
     if(scroll == 0 && scrollAdded){
         while(style.sheet.cssRules.length > 0){
-            style.sheet.removeRule(0);
+            style.sheet.deleteRule(0);
         }
         scrollAdded = false
     }
     else if(scroll != 0 && !scrollAdded){
-        style.sheet.insertRule('#header{height:40px;}', 0);
+        style.sheet.insertRule('#header{height:40px !important;}', 0);
         style.sheet.insertRule('#znak{max-height: 40px}', 0);
         style.sheet.insertRule('#linky{height: 40px}', 0);
-        style.sheet.insertRule('.headButton{margin:0 !important; }', 0);
+        style.sheet.insertRule('.headButton{margin:1px !important; padding:5px !important;}', 0);
         
         scrollAdded=true;
     }
